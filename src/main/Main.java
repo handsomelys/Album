5a34192e70ab9a691fd20f28662cceeca2573226
lsn42 <24622966+lsn42@users.noreply.github.com>
2020-03-17 20:46:44 +0800

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String args[]) {
+        JFrame jf = new JFrame("picture");
+        jf.setSize(400, 300);
+        jf.setVisible(true);
+    }
+}
